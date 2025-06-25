5c322c6577251e18f17708a35cf4a07e19c72fcd
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-06-25 14:36:17 -0300

    feat: implement endpoint CreatUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import type { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Ana',
+      email: 'anaclara@gmail.com',
+    },
+  ];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const id = this.users[this.users.length - 1].id + 1;
+
+    const user: User = {
+      ...createUserDto,
+      id,
+    };
+
+    this.users.push(user);
+    return user;
   }
 
   findAll() {
